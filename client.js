2cb8bbdda5840ec49d4d8e785410830503bb4401
Jonathan Phair <jmphair@gmail.com>
2023-01-18 14:29:17 -0500

    added name and server says Hey

--- client.js
+++ client.js
@@ -12,6 +12,8 @@ const connect = function () {
 
   conn.on('connect', () => {
     console.log('Successfully connected to game server!');
+    // from the article I think it's this one... ran with console.log a few too many times lol
+    conn.write('Name: JP'); // working! the servers says: "JP: Hey"
   });
 
   return conn;
