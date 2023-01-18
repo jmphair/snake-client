75f37d841c93efedb5a8da052a0fa4bc220cd5f7
Jonathan Phair <jmphair@gmail.com>
2023-01-18 16:28:26 -0500

    got the constants to work after a little snafu

--- client.js
+++ client.js
@@ -1,10 +1,11 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
 
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: "192.168.2.40",// IP address here,
-    port: 50541 // PORT number here,
+    host: IP,
+    port: PORT
   });
 
   // interpret incoming data as text
---
+++ constants.js
@@ -0,0 +1,10 @@
+const IP = '192.168.2.40';
+
+const PORT = 50541;
+
+module.exports = {
+  
+  IP,
+  PORT
+
+}
