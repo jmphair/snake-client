9d62cd7382213c0ee0271771a1e0d4785f105328
Jonathan Phair <jmphair@gmail.com>
2023-01-18 15:12:50 -0500

    managed to add the ctrl c functionality

--- client.js
+++ client.js
@@ -20,8 +20,3 @@ const connect = function () {
 };
 
 module.exports = connect;   
-
-// "Move: up" - move up one square (unless facing down)
-// "Move: down" - move down one square (unless facing up)
-// "Move: left" - move left one square (unless facing right)
-// "Move: right" - move left one square (unless facing left)
