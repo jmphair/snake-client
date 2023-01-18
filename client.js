016390b8bd1a1d387a34ac23a025a596c20fe01a
Jonathan Phair <jmphair@gmail.com>
2023-01-18 14:42:20 -0500

    ok move up works in the conn object

--- client.js
+++ client.js
@@ -14,6 +14,7 @@ const connect = function () {
     console.log('Successfully connected to game server!');
     // from the article I think it's this one... ran with console.log a few too many times lol
     conn.write('Name: JP'); // working! the servers says: "JP: Hey"
+    conn.write('Move: up'); // it works to move it up one level!
   });
 
   return conn;
