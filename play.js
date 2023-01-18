8fab15847f7dbf0cb9967e2ba564bea04c280afa
Jonathan Phair <jmphair@gmail.com>
2023-01-18 13:59:07 -0500

    added connect

--- play.js
+++ play.js
@@ -3,12 +3,16 @@ const net = require("net");
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: // IP address here,
-    port: // PORT number here,
+    host: "192.168.2.40",// IP address here,
+    port: 50541 // PORT number here,
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+
+  conn.on('connect', () => {
+    console.log('you ded cuz you idled');
+  });
 
   return conn;
 };
