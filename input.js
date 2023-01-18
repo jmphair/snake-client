c97a78c8edb1f6e5dd830b2af59e2063cdd97dc6
Jonathan Phair <jmphair@gmail.com>
2023-01-18 15:32:36 -0500

    initial commit, and export setupInput

---
+++ input.js
@@ -0,0 +1,28 @@
+// "Move: up" - move up one square (unless facing down)
+// "Move: down" - move down one square (unless facing up)
+// "Move: left" - move left one square (unless facing right)
+// "Move: right" - move left one square (unless facing left)
+
+// setup interface to handle user input from stdin
+
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function () {
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+};
+
+module.exports = {
+  setupInput
+};
